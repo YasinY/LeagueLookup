38b28f50628176b670340a4d033525911e4efa47
Yasin Yazici <yasin_programmer@hotmail.com>
2017-02-20 20:59:31 +0100

    Data now appends, TODO: convert to js

--- src/com/lollookup/ActiveGameController.java
+++ src/com/lollookup/ActiveGameController.java
@@ -2,12 +2,11 @@ package com.lollookup;
 
 import com.google.common.base.Charsets;
 import com.google.common.io.Files;
-import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Worker;
 import javafx.fxml.FXML;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
-import javax.swing.plaf.nimbus.State;
 import java.io.IOException;
 
 public class ActiveGameController {
@@ -30,21 +29,21 @@ public class ActiveGameController {
         final WebEngine webEngine = webView.getEngine();
         webEngine.loadContent(Files.toString(Config.ACTIVE_GAME_HTML, Charsets.UTF_8));
         webEngine.getLoadWorker().stateProperty().addListener(
-                new ChangeListener<State>() {
-                    public void changed(ObservableValue ov, State oldState, State newState) {
-                        if (newState == Worker.State.SUCCEEDED) {
-                            webengine.executeScript("document.getElementById('header').innerHTML = '<p>Hi</p>';");
-                        }
+                (observable, oldValue, newValue) -> {
+                    if (newValue == Worker.State.SUCCEEDED) {
+                        webEngine.executeScript("var redTeam = document.getElementById('redTeam');" +
+                                "redTeam.insertRow(1).insertCell(0).innerHTML='TEST';" +
+                                "redTeam.insertRow(2).insertCell(0).innerHTML='TEST';");
                     }
                 });
     }
 
     private void appendDataToTable() {
     }
+
     private void executeScript(String script) {
         webView.getEngine().executeScript(script);
     }
-
 
 
 }
